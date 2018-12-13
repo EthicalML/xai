fcbe653520de60f7f5001300182dde7bb0996466
alejandro <axsauze@gmail.com>
2018-12-13 20:02:06 +0000

    feat: added

--- README.md
+++ README.md
@@ -44,21 +44,38 @@ from xai.xdata import XData
 from xai import xmodel
 from xai import xmonitor
 
-xd = XData("path/to/csv/data.csv")
+xd = XData("path/to/csv/data.csv", "target_col")
+#> Loads file, converts numbers to numerical, short str to categorical, long str to text (100 char+)
 
 xd.df.head()
 #> Current DF
 
-xd.buckets()
-#> Show buckets of all
+xd.imbalances(threshold=0.5, plot=True)
+#> Show buckets of all features
+
+xd.target_imbalances()
+#> Show buckets of all features as breakdown to the target function
+
+xd.partial_imbalances()
+#> Show buckets of selected features compared across each other
+
+
 
 xd.correlations()
 #> Show correlations of all
 
-xd.convert_categorical(["col1", "col2", "col3"])
-
 xd.display.head()
 #> DF with unmodified values
+
+print(xd.x_train.shape, xd.y_train.shape)
+#> Balanced on 80% by default
+
+print(xd.x_test.shape, xd.y_test.shape)
+#> Balanced on 20% by default
+
+xd.train_test_split(balanced_columns=["col1", "col2"])
+#> Plot showing how it is balanced on those two classes
+
 
 ```
 
--- xai/xdata.py
+++ xai/xdata.py
@@ -1,5 +1,50 @@
+import pandas as pd
+import matplotlib as plt
+from typing import List, Any
 
 class XData:
-    def __init__(self, csv_file: str) -> None:
-        self._csv_file = csv_file
+    def __init__(self, 
+                label_column: str,
+                csv_path: str,
+                df: pd.DataFrame = None,
+                col_names: List[str] = None,
+                categorical_cols: List[str] = None,
+                string_cols: List[str] = None,
+                **kwargs) -> None:
+        
+        if df is not None:
+            if not isinstance(df, pd.DataFrame):
+                raise Exception("df parameter provided, but not of type dataframe.")
+        else:
+            if not csv_path:
+                raise Exception("Neither df or csv_file parameters provided.")
+            df = self._read_csv(csv_path, col_names, **kwargs)
+
+        self._initialise_from_dataframe(df, col_names, categorical_cols)
+
+    def _initialise_from_dataframe(self,
+            df: pd.DataFrame,
+            col_names: List[str] = None,
+            categorical_cols: List[str] = None) -> None:
+        """
+        Process dataframe provided to create XData object from it. 
+        """
+        if col_names:
+            df.columns = col_names
+
+        self.df = df
+
+        
+
+    def _read_csv(self, 
+            csv_path: str, 
+            col_names: List[str] = None, 
+            **kwargs) -> pd.DataFrame:
+        """
+        Read dataframe from csv to pandas dataframe and return result.
+        """
+
+        df = pd.read_csv(csv_path, names=col_names, **kwargs)
+        return df
+
 
