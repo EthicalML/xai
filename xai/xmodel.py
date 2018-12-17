8899c126fc82d1f2b5633b9df347722d4f7b5854
alejandro <axsauze@gmail.com>
2018-12-17 14:04:54 +0000

    updated monitor and model

--- xai/xmodel.py
+++ xai/xmodel.py
@@ -1,5 +1,15 @@
 
+from xai.xdata import XData
+
+import pandas as pd
+from typing import List, Any, Callable
+
 class XModel:
-    def __init__(self, xdata: XData) -> None:
+    def __init__(self, 
+            xdata: XData,
+            trained_model: Any,
+            predict_func: Any = lambda x,y,m: m.predict(x)) -> None:
         self._xdata = xdata
+        self._model = trained_model
+
 
--- xai/xmonitor.py
+++ xai/xmonitor.py
@@ -1,5 +1,8 @@
 
 class XMonitor:
-    def __init__(self, xmodel: XModel) -> None:
-        self._xmodel = xmodel
+    def __init__(self, 
+            model: Any,
+            predict_func: Any = lambda x,y,m: m.predict(x)) -> None:
+        self._model = trained_model
+
 
