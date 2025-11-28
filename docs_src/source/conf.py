50d9a7d3adbd899672e2a0d23435bc62592257a5
Alejandro Saucedo <alejandro.saucedo@zalando.de>
2025-11-28 22:36:18 +0100

    feat: migrate to uv

--- docs_src/source/conf.py
+++ docs_src/source/conf.py
@@ -32,8 +32,8 @@ print(sys.path)
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc', 
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
     'sphinx_autodoc_typehints',
 ]
 
@@ -70,7 +70,7 @@ release = '0.1'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
